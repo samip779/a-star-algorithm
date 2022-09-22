b0416901ecf3ae20c6125394c6ecfe7050da46df
samip779 <samipluitel28@gmail.com>
2022-09-22 20:37:03 +0545

    formatted first program

--- pathfinding_task1.py
+++ pathfinding_task1.py
@@ -17,9 +17,8 @@ def reconstruct_path(came_from, current):
     return reversed_path
 
 
-
 # this function calculates heuristic value using Manhatten's distance formula
-def heuristic(p1: Location,p2: Location) -> int:
+def heuristic(p1: Location, p2: Location) -> int:
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1-x2) + abs(y1-y2)
@@ -28,26 +27,32 @@ def heuristic(p1: Location,p2: Location) -> int:
 def find_neighbours(state: Location, terrain_map: Map, terrain_threshold: int, f_score):
     row, column = state
     neighbours = []
-    
-    if row < len(terrain_map) - 1 and terrain_map[row + 1][column] < terrain_threshold: #DOWN
+
+    # DOWN
+    if row < len(terrain_map) - 1 and terrain_map[row + 1][column] < terrain_threshold:
         neighbours.append((row + 1, column))
-    elif row < len(terrain_map) - 1 and terrain_map[row + 1][column] >= terrain_threshold: log_ignore_state((row + 1, column), f_score[row+1, column])
-    
-    if row > 0 and terrain_map[row-1][column] < terrain_threshold: #UP
+    elif row < len(terrain_map) - 1 and terrain_map[row + 1][column] >= terrain_threshold:
+        log_ignore_state((row + 1, column), f_score[row+1, column])
+
+    # UP
+    if row > 0 and terrain_map[row-1][column] < terrain_threshold:
         neighbours.append((row - 1, column))
-    elif row > 0 and terrain_map[row - 1][column] >= terrain_threshold: log_ignore_state((row - 1, column), f_score[row-1, column])
+    elif row > 0 and terrain_map[row - 1][column] >= terrain_threshold:
+        log_ignore_state((row - 1, column), f_score[row-1, column])
 
-    if column < len(terrain_map[row]) -1 and terrain_map[row][column + 1] < terrain_threshold: #LEFT
+    # LEFT
+    if column < len(terrain_map[row]) - 1 and terrain_map[row][column + 1] < terrain_threshold:
         neighbours.append((row, column + 1))
-    elif column < len(terrain_map[row]) -1 and terrain_map[row][column + 1] >= terrain_threshold: log_ignore_state((row, column + 1), f_score[row, column + 1])
+    elif column < len(terrain_map[row]) - 1 and terrain_map[row][column + 1] >= terrain_threshold:
+        log_ignore_state((row, column + 1), f_score[row, column + 1])
 
-    if column > 0 and terrain_map[row][column - 1] < terrain_threshold: #RIGHT
+    # RIGHT
+    if column > 0 and terrain_map[row][column - 1] < terrain_threshold:
         neighbours.append((row, column - 1))
-    elif column > 0 and terrain_map[row][column - 1] >= terrain_threshold: log_ignore_state((row, column - 1), f_score[row, column - 1])
+    elif column > 0 and terrain_map[row][column - 1] >= terrain_threshold:
+        log_ignore_state((row, column - 1), f_score[row, column - 1])
 
     return neighbours
-
-
 
 
 def algorithm(start: Location, goal: Location, terrain_map: Map, terrain_threshold: int):
@@ -60,26 +65,17 @@ def algorithm(start: Location, goal: Location, terrain_map: Map, terrain_thresho
     g_score = {}
     f_score = {}
 
-
     for row in range(len(terrain_map)):
         for col in range(len(terrain_map[row])):
             g_score[(row, col)] = float("inf")
 
-
-
     g_score[start] = 0
-
 
     for row in range(len(terrain_map)):
         for col in range(len(terrain_map[row])):
             f_score[(row, col)] = float("inf")
 
-
-
-
     f_score[start] = heuristic(start, goal)
-
-
 
     frontier_hash = {start}
 
@@ -94,8 +90,9 @@ def algorithm(start: Location, goal: Location, terrain_map: Map, terrain_thresho
             return [g_score[goal], result]
 
         for neighbour in find_neighbours(current, terrain_map, terrain_threshold, f_score):
-            x,y = neighbour
-            temp_g_score = g_score[current] + terrain_map[current[0]][current[1]] + terrain_map[x][y]
+            x, y = neighbour
+            temp_g_score = g_score[current] + \
+                terrain_map[current[0]][current[1]] + terrain_map[x][y]
             if temp_g_score < g_score[neighbour]:
                 came_from[neighbour] = current
                 g_score[neighbour] = temp_g_score
@@ -109,16 +106,9 @@ def algorithm(start: Location, goal: Location, terrain_map: Map, terrain_thresho
     return None
 
 
-
-
-
-
-
-
-
 def find_shortest_path(start: Location, goal: Location,
                        terrain_map: Map, terrain_threshold: int) \
-                   -> tuple[Optional[int],Optional[list[Location]]]:
+        -> tuple[Optional[int], Optional[list[Location]]]:
     """Finds the path with lowest total cost (Task 1)
        Returns (cost,list(locations)) when a path is found.
        Returns (None,None) if no path is found."""
@@ -131,14 +121,11 @@ def find_shortest_path(start: Location, goal: Location,
     return algorithm(start, goal, terrain_map, terrain_threshold)
 
 
-
-
-
 @click.command(no_args_is_help=True)
 @click.argument('start', required=True, callback=validate_location)
 @click.argument('goal', required=True, callback=validate_location)
 @click.argument("terrain_map", required=True, type=click.Path(exists=True), callback=validate_map)
-@click.argument("terrain_threshold", required=True, type=click.IntRange(min=0,max=1000))
+@click.argument("terrain_threshold", required=True, type=click.IntRange(min=0, max=1000))
 def main(start: Location, goal: Location, terrain_map: Map, terrain_threshold: int) -> None:
     """Example usage:
 
@@ -150,6 +137,7 @@ def main(start: Location, goal: Location, terrain_map: Map, terrain_threshold: i
         log(f"The path is {path[1]} with cost {path[0]}.")
     else:
         log('No path found')
+
 
 if __name__ == '__main__':
     main()
